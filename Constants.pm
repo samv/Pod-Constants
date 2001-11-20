9f6e57064487cb567818513c2ae80434bdf7d17e
Sam Vilain <sam@vilain.net>
2001-11-20 18:26:55 +0000

    Imported from Pod-Constants-0.11.tar.gz.

--- Constants.pm
+++ Constants.pm
@@ -1,3 +1,7 @@
+# Copyright (C) 2001 Sam Vilain.  All Rights Reserved.  This module is
+# free software. It may be used, redistributed and/or modified under the
+# terms of the Perl Artistic License
+
 package Pod::Constants;
 
 =head1 NAME
@@ -6,30 +10,70 @@ Pod::Constants - Include constants from POD
 
 =head1 SYNOPSIS
 
- use vars qw($myvar);
+ use vars qw($myvar $VERSION @myarray $html %myhash);
+
  use Pod::Constants -trim => 1,
-     'Pod Section Name' => \$myvar;
+     'Pod Section Name' => \$myvar,
+     'Version' => sub { eval },
+     'Some list' => \@myarray,
+     html => \$html,
+     'Some hash' => \%myhash;
 
  =head2 Pod Section Name
 
  This string will be loaded into $myvar
 
+ =head2 Version
+
+ # This is an example of using a closure.  $_ is set to the
+ # contents of the paragraph.  In this example, "eval" is
+ # used to execute this code at run time.
+ $VERSION = 0.11;
+
+ =head2 Some list
+
+ Each line from this section of the file
+ will be placed into a seperate array element.
+ For example, this is $myarray[2].
+
+ =head2 Some hash
+
+ This text will not go into the hash, because
+ it doesn't look like a definition list.
+     key1 => Some value (this will go into the hash)
+     var2 => Some Other value (so will this)
+     wtf = This won't make it in.
+
+ =head2 %myhash's value after the above:
+
+    ( key1 => "Some value (this will go into the hash)",
+      var2 => "Some Other value (so will this)"          )
+
+ =begin html <p>This text will be in $html</p>
+
  =cut
 
 =head1 DESCRIPTION
 
-This module is for when you have constants in your code, but you want
-to keep them in the documentation so that when they change you only
-have to update them in one place.
+This module allows you to specify those constants that should be
+documented in your POD, and pull them out a run time in a fairly
+arbitrary fashion.
 
 Pod::Constants uses Pod::Parser to do the parsing of the source file.
 It has to open the source file it is called from, and does so directly
-either by lookup in %INC or from $0 if the caller is "main".
+either by lookup in %INC or by assuming it is $0 if the caller is
+"main" (or it can't find %INC{caller()})
+
+=head2 ARBITARY DECISIONS
 
 I have made this code only allow the "Pod Section Name" to match
-`head1', `head2' and `item' POD sections.  If you have a good reason
-why you think it should match other POD sections, drop me a line and
-if I'm convinced I'll put it in the standard version.
+`headN', `item', `for' and `begin' POD sections.  If you have a good
+reason why you think it should match other POD sections, drop me a
+line and if I'm convinced I'll put it in the standard version.
+
+For `for' and `begin' sections, only the first word is counted as
+being a part of the specifier, as opposed to `headN' and `item', where
+the entire rest of the line counts.
 
 =cut
 
@@ -38,24 +82,75 @@ use strict;
 
 use base qw(Pod::Parser Exporter);
 use Data::Dumper;
+use Carp;
 
 # Global parser state variables
-use vars qw(%wanted_pod_tags %trim $active $VERSION);
+use vars qw(%wanted_pod_tags %trim $active $VERSION $paragraphs $DEBUG);
 
-$VERSION = "0.02";
+sub end_input {
+    #my ($parser, $command, $paragraph, $line_num) = (@_);
+
+    return unless $active;
+
+    print "Found end of $active\n" if ($DEBUG);
+    my $whereto = $wanted_pod_tags{$active};
+    print "\$_ will be set to:\n---\n$paragraphs\n---\n" if ($DEBUG);
+
+    if (ref $whereto eq "CODE") {
+	print "calling sub\n" if $DEBUG;
+	local ($_) = $paragraphs;
+	$whereto->();
+	print "done\n" if $DEBUG;
+    } elsif (ref $whereto eq "SCALAR") {
+	print "inserting into scalar\n" if $DEBUG;
+	$$whereto = $paragraphs;
+    } elsif (ref $whereto eq "ARRAY") {
+	print "inserting into array\n" if $DEBUG;
+	@$whereto = split /\n/, $paragraphs;
+    } elsif (ref $whereto eq "HASH") {
+	print "inserting into hash\n" if $DEBUG;
+	# Oh, sorry, should I be in LISP101?
+	%$whereto = (map { map { s/^\s*|\s*$//g; $_ }
+			       split /=>/, $_ }
+		     grep m/^
+			    ( (?:[^=]|=[^>])+ )   # scan up to "=>"
+			    =>
+			    ( (?:[^=]|=[^>])+ =? )# don't allow more "=>"'s
+			    $/x,
+		     split /\n/, $paragraphs);
+    } else { die $whereto }
+    $active = undef;
+}
 
 # Pod::Parser overloaded command
 sub command {
     my ($parser, $command, $paragraph, $line_num) = @_;
 
     $paragraph =~ s/^\s*|\s*$//gs;
+    end_input() if $active;
 
-    if ($command =~ m/^(head[12]|item)$/) {
+    my $does_she_want_it_sir;
+    if ($command =~ m/^(head\d+|item)$/) {
 	if (exists $wanted_pod_tags{$paragraph}) {
+	    print "Found $paragraph\n" if ($DEBUG);
 	    $active = $paragraph;
+	    $paragraphs = undef;
+	    $does_she_want_it_sir = "suits you sir";
+	}
+    } elsif ($command =~ m/^(for|begin)$/) {
+	(my $section, $paragraphs) =
+	    ($paragraph =~ m/^\s*(\S*)\s*(.*)/s);
+	if (exists $wanted_pod_tags{$section}) {
+	    print "Found $section\n" if ($DEBUG);
+	    $active = $section;
+	    $does_she_want_it_sir = "suits you sir";
 	}
     } else {
-	$active = undef;
+	# nothing
+    }
+    {local $^W = 0;
+    print "Ignoring =$command $paragraph\n"
+	if (!$does_she_want_it_sir and $DEBUG)
     }
 }
 
@@ -65,8 +160,7 @@ sub verbatim {
 
     if (defined $active) {
 	$paragraph =~ s/^\s*|\s*$//gs if $trim{$active};
-	${$wanted_pod_tags{$active}} = $paragraph;
-	$active = undef;
+	$paragraphs .= $paragraph;
     }
 }
 
@@ -80,36 +174,45 @@ sub import {
     # try to guess the source file of the caller
     my $source_file;
     if (caller ne "main") {
-    	print "Caller: ".caller()."\n";
-	my $module = caller;
-	$module =~ s|::|/|g;
-	$module .= ".pm";
-    	print "module: ".$module."\n";
+	(my $module = caller().".pm") =~ s|::|/|g;
 	$source_file = $INC{$module};
-    	print "inc(.): ".$source_file."\n";
     }
-    print "\$0: $0\n";
     $source_file ||= $0;
-    print "final source file: $source_file\n";
 
-    my $parser = $class->new();
-    open CLASSFILE, "<$source_file"
-	or die "cannot open $source_file for reading; $!";
+    ( -f $source_file )
+	or croak ("Cannot find source file (guessed $source_file) for"
+		  ." package ".caller());
+
+    import_from_file($source_file, @args);
+}
+
+sub import_from_file {
+    my ($filename, @what) = (@_);
+
+    my $parser = __PACKAGE__->new();
+    open CLASSFILE, "<$filename"
+	or die ("cannot open $filename for reading; $!");
 
     %wanted_pod_tags = %trim = ();
-    $active = undef;
-    my ($trim);
 
-    while (my ($pod_tag, $var) = splice @args, 0, 2) {
-	if (lc($pod_tag) eq "-trim") {
+    $active = undef;
+    my ($trim, $get_usage);
+
+    # find out what they want imported where
+    while (my ($pod_tag, $var) = splice @what, 0, 2) {
+	if (lc($pod_tag) =~ "-trim") {
 	    $trim = $var;
+	} elsif (lc($pod_tag) =~ "-usage") {
+	    # an idea for later - automatic "usage"
+	    #%wanted_pod_tags{@tags}
 	} else {
-	    if (ref $var eq "SCALAR") {
+	    if ((ref $var) =~ /^(?:SCALAR|CODE|ARRAY|HASH)$/) {
 		$wanted_pod_tags{$pod_tag} = $var;
 		$trim{$pod_tag} = 1 if $trim;
 	    } else {
-		die ("Sorry - can only import POD sections into scalars "
-		     ."importing $pod_tag into ".caller);
+		die ("Sorry - need a plain reference to import POD "
+		     ."sections into, not a ref ".(ref $var)
+		     ." importing $pod_tag into ".caller());
 	    }
 	}
     }
@@ -119,33 +222,110 @@ sub import {
     close CLASSFILE;
 }
 
-=head1 AUTO MODULE VERSIONS
+=head2 CLOSURES AS DESTINATIONS
 
-Put this in your module code for automatic POD/Makefile.PL updating:
+If the given value is a ref CODE, then that function is called, with
+$_ set to the value of the paragraph.  This can be very useful for
+applying your own custom mutations to the POD to change it from human
+readable text into something your program can use.
 
-   =head2 MODULE RELEASE
+After I added this function, I just kept on thinking of cool uses for
+it.  The nice, succinct code you can make with it is one of
+Pod::Constant's strongest features.
 
-   $VERSION = 1.05
+Below are some examples.
 
-   =cut
+=head1 EXAMPLES
 
-   use vars qw($VERSION);
-   use Pod::Constants -trim => 1, 'MODULE RELEASE' => \$VERSION;
-   BEGIN { $VERSION =~ s/^\$VERSION\s*=\s*// };
+=head2 Module Makefile.PL maintenance
+
+Tired of keeping those module Makefile.PL's up to date?
+
+=head2 Example Makefile.PL
+
+ eval "use Pod::Constants";
+ ($Pod::Constants::VERSION >= 0.11)
+     or die <<EOF
+ ####
+ ####  ERROR: This module requires Pod::Constants 0.11 or
+ ####  higher to be installed.
+ ####
+ EOF
+
+ my ($VERSION, $NAME, $PREREQ_PM, $ABSTRACT, $AUTHOR);
+ Pod::Constants::import_from_file 
+     (
+      'MyTestModule.pm', -trim => 1,
+      'NAME' => sub { $ABSTRACT=$_; ($NAME) = m/(\S+)/ },
+      'MODULE RELEASE' => sub { ($VERSION) = m/(\d+\.\d+)/ },
+      'DEPENDANCIES' => ($PREREQ_PM = { }),
+      'AUTHOR' => \$AUTHOR,
+     );
+
+ WriteMakefile
+     (
+      'NAME'		=> $NAME,
+      'PREREQ_PM'        => $PREREQ_PM,
+      'VERSION'          => $VERSION,
+      ($] >= 5.005 ?    ## Add these new keywords supported since 5.005
+       (ABSTRACT         => $ABSTRACT,
+        AUTHOR           => $AUTHOR) : ()),
+     );
+
+=head2 Corresponding Module
+
+ =head1 NAME
+
+ MyTestModule - Demonstrate Pod::Constant's Makefile.PL usefulness
+
+ =head2 MODULE RELEASE
+
+ This is release 1.05 of this module.
+
+ =head2 DEPENDANCIES
+
+ The following modules are required to make this module:
+
+    Some::Module => 0.02
+
+ =head2 AUTHOR
+
+ Ima Twat <ima@twat.name>
+
+ =cut
+
+ use vars qw($VERSION);
+ use Pod::Constants -trim => 1,
+     'MODULE RELEASE' => sub { ($VERSION) = m/(\d+\.\d+) or die };
+
+=head1 
 
 =head1 AUTHOR
 
-Sam Vilain, <sam@vilain.net>
+Sam Vilain, <perl@snowcra.sh>
 
 =head1 BUGS/TODO
 
-Is there any value in being able to import structured data from POD
-sections, perhaps?  Maybe extracting tabular information into arrays
-or hashes?
+I keep thinking it would be nice to be able to import an =item list
+into an array or something, eg for a program argument list.  But I'm
+not too sure how it would be all that useful in practice; you'd end up
+putting the function names for callbacks in the pod or something
+(perhaps not all that bad).
 
-What about doing nasty things to the caller's symbol table, so they
-don't need to "use vars"?
+Would this be useful?
+
+ Pod::Constants::import(Foo::SECTION => \$myvar);
 
 =cut
 
-1;
+BEGIN {
+    # 
+    Pod::Constants->import
+	    (
+	     SYNOPSIS => sub {
+		 eval pop @{[ grep /^\s*\$VERSION/, split /\n/, $_ ]}
+	     }
+	    )
+};
+
+1.4142;
--- t/01-constants.t
+++ t/01-constants.t
@@ -1,16 +1,22 @@
 #!/usr/bin/perl -w
 
 use strict;
-use Test::More tests => 6;
+use Test::More tests => 17;
+use Data::Dumper;
 
-use vars qw($section_1 $section_2);
+use vars qw($section_1 $section_2 $section_3 $section_4);
 
 use_ok(
        "Pod::Constants",
        section_1 => \$section_1,
        -trim => 1,
-       section_2 => \$section_2
+       section_2 => \$section_2,
+       section_3 => sub { tr/[a-z]/[A-Z]/; $section_3 = $_ },
+       section_4 => sub { eval },
       );
+
+ok($Pod::Constants::VERSION,
+   "Pod::Constants sets its own VERSION");
 
 BEGIN {
     push @INC, "t";
@@ -19,9 +25,50 @@ use Cheese;
 
 is($section_1, "Legalise Cannabis!\n\n", "no trim from main");
 is($section_2, "42", "with trim from main");
+is($section_3, "STICKY BUD", "sub");
+is($section_4, "hash cookies", "eval");
 is($Cheese::foo, "detcepxe", "From module");
 like(`perl -c t/Cheese.pm 2>&1`, qr/syntax OK/, "perl -c module");
 like(`perl -c t/cheese.pl 2>&1`, qr/syntax OK/, "perl -c script");
+
+# test the examples on the man page :)
+package Pod::Constants;
+Pod::Constants->import (SYNOPSIS => sub {
+    $main::section_1 = join "\n", map { s/^ //; $_ } split /\n/, $_
+});
+
+package main;
+open NEWPKG, ">t/TestManPage.pm" or die $!;
+# why define your test results when you can read them in from POD?
+$section_1 =~ s/myhash\)/myhash %myhash2)/;
+$section_1 =~ s/myhash;/myhash, "%myhash\'s value after the above:" => sub { %myhash2 = eval };/;
+print NEWPKG "package TestManPage;\n$section_1\n2.818;\n";
+close NEWPKG;
+
+use_ok("TestManPage");
+
+is($TestManPage::myvar, 'This string will be loaded into $myvar',
+   "man page example 1");
+is($TestManPage::VERSION, $Pod::Constants::VERSION,
+   "man page example 2");
+ok($TestManPage::VERSION,
+   "man page example 2 cross-check");
+is($TestManPage::myarray[2], 'For example, this is $myarray[2].',
+   "man page example 3");
+my $ok = 0;
+while (my ($k, $v) = each %TestManPage::myhash) {
+    if (exists $TestManPage::myhash2{$k}) { $ok ++ };
+    if ($v eq $TestManPage::myhash2{$k}) { $ok ++ };
+}
+is($ok, 4,
+   "man page example 4");
+is(scalar keys %TestManPage::myhash, 2,
+   "man page example 4 cross-check");
+is($TestManPage::html, '<p>This text will be in $html</p>',
+   "man page example 5");
+# supress warnings
+$TestManPage::myvar = $TestManPage::html = undef;
+@TestManPage::myarray = ();
 
 =head2 section_1
 
@@ -30,5 +77,13 @@ Legalise Cannabis!
 =head2 section_2
 
 42
+
+=head2 section_3
+
+sticky bud
+
+=head2 section_4
+
+$section_4 = "hash cookies"
 
 =cut
